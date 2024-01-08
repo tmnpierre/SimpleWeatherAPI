7e165607d0ceba254ce19207cb1a3c3254c5aa93
tmnpierre <tomyn.pierre@gmail.com>
2024-01-08 19:27:11 +0100

    feat(weather-controller): add new API controller and secure API key retrieval

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -1,6 +1,30 @@
-﻿namespace SimpleWeatherAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherService.Controllers
 {
-    public class WeatherController
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherController : ControllerBase
     {
+        [HttpGet("current")]
+        public async Task<IActionResult> GetCurrentWeather()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("WEATHER_API_KEY");
+            string url = $"https://api.openweathermap.org/data/2.5/weather?q=Paris&appid={apiKey}";
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonData = await response.Content.ReadAsStringAsync();
+                    return Ok(jsonData);
+                }
+                else
+                {
+                    return StatusCode((int)response.StatusCode, "Error calling the weather service");
+                }
+            }
+        }
     }
 }
